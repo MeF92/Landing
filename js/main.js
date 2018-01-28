412b687330cd947d6b8a50c412966ed87bbbb3bf
Denis Bozhko <Mefisto-92@mail.ru>
2018-01-28 20:06:38 +0300

    macbook

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
 'use strict';
 
+var toggles = document.querySelectorAll('.toggle');
+var img1 = document.querySelector('.macbook img:first-child');
+var img2 = document.querySelector('.macbook img:nth-child(2)');
+var img3 = document.querySelector('.macbook img:last-child');
+
+var onToggleClick = function(evt) {
+    evt.preventDefault();
+    evt.target.style.borderColor = '#59bfe6';
+};
+
+toggles[0].addEventListener('click', function (evt) {
+    onToggleClick(evt);
+    toggles[1].style.borderColor = '#e4e5e5';
+    toggles[2].style.borderColor = '#e4e5e5';
+   img1.classList.remove('hidden');
+   img2.classList.add('hidden');
+   img3.classList.add('hidden');
+});
+
+toggles[1].addEventListener('click', function (evt) {
+    onToggleClick(evt);
+    toggles[0].style.borderColor = '#e4e5e5';
+    toggles[2].style.borderColor = '#e4e5e5';
+    img1.classList.add('hidden');
+    img2.classList.remove('hidden');
+    img3.classList.add('hidden');
+});
+
+toggles[2].addEventListener('click', function (evt) {
+    onToggleClick(evt);
+    toggles[0].style.borderColor = '#e4e5e5';
+    toggles[1].style.borderColor = '#e4e5e5';
+    img1.classList.add('hidden');
+    img2.classList.add('hidden');
+    img3.classList.remove('hidden');
+});
